56180170f250e25168b8bc2aa9650f5d931e5cb8
Kevin <58088497+0x534b@users.noreply.github.com>
2022-05-28 13:02:08 -0400

    heroku test

---
+++ app/image_processing.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from translate import Blurb
+
+import sys
+import numpy as np
+import dill as pickle
+import cv2
+
+import pytesseract
+
+if __name__ == '__main__':
+  img = cv2.imread(  )
+  get_blurbs(img)
+
--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from translate import Translator
+
+from urllib import request
+import tempfile
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "<h1>boblov</h1>"
+    return f"<h1>out</h1>"
+
+    # tempthing = request.urlopen(url)
+
+    # translator= Translator(from_lang="japanese",to_lang="english")
+    # boblov = translator.translate("めんどくさい")
+    # return f"<h1>{boblov}</h1>"
